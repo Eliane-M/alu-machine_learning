89faf6cf2589a62d384822916ecdcbf168f6d1d0
Eliane_M <munezeroeliane761@gmail.com>
2024-09-10 22:43:01 +0200

    algebra work

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -4,7 +4,8 @@
     It takes a 2D list (matrix) as input,
     and returns its transpose.
 
-    The transpose of a matrix is obtained by swapping its rows with its columns.
+    The transpose of a matrix is obtained by
+    swapping its rows with its columns.
 """
 
 
@@ -13,10 +14,10 @@ def matrix_transpose(matrix):
     This function takes a 2D list (matrix)
     as input and returns its transpose.
     """
-    transpose = [[0 for _ in range(len(matrix))] for _ in range(len(matrix[0]))]
+    transpose = [[0 for _ in range(len(matrix))]
+                 for _ in range(len(matrix[0]))]
 
     for j in range(len(matrix)):
         for i in range(len(matrix[0])):
             transpose[i][j] = matrix[j][i]
-
     return transpose
