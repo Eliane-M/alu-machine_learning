f4662bb777d5383446c3210e3f16264b77a751df
Eliane_M <munezeroeliane761@gmail.com>
2024-09-11 20:03:45 +0200

    algebra work

--- math/linear_algebra/11-the_western_exchange.py
+++ math/linear_algebra/11-the_western_exchange.py
@@ -4,9 +4,8 @@ This function transposes a matrix
 """
 
 
-import numpy as np
 def np_transpose(matrix):
     """
     Transposes a matrix
     """
-    return np.transpose(matrix)
+    return matrix.transpose()
