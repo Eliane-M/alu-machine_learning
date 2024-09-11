91a4cbc1bd2720f58d1c5238036dd5fa8f48af11
Eliane_M <munezeroeliane761@gmail.com>
2024-09-11 20:02:38 +0200

    algebra work

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+This function transposes a matrix
+"""
+
+
+import numpy as np
+def np_transpose(matrix):
+    """
+    Transposes a matrix
+    """
+    return np.transpose(matrix)
