23a09508463ab4b6a329293d24a87e8ae2484240
Eliane_M <munezeroeliane761@gmail.com>
2024-09-11 17:20:53 +0200

    algebra work

--- math/linear_algebra/4-line_up.py
+++ math/linear_algebra/4-line_up.py
@@ -9,10 +9,10 @@ def add_arrays(arr1, arr2):
     This function adds two arrays together
     """
     if len(arr1) != len(arr2):
-            return None
-    
+        return None
+
     result = []
     for i in range(len(arr1)):
         result.append(arr1[i] + arr2[i])
-        
+  
     return result
