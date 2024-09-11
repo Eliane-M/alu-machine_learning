f41378c7403022b23cda6bb1d34e3b699bea94f5
Eliane_M <munezeroeliane761@gmail.com>
2024-09-11 19:53:50 +0200

    algebra work

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+calculates the shape of a numpy.ndarray
+"""
+
+
+def np_shape(matrix):
+    """
+    Returns the shape of the numpy.ndarray matrix
+    """
+    return matrix.shape
