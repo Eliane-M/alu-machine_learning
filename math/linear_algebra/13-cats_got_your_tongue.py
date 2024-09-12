cd4cc2c8f92fb03db235c37830adddbc146da668
Eliane_M <munezeroeliane761@gmail.com>
2024-09-12 08:23:47 +0200

    algebra work

--- math/linear_algebra/13-cats_got_your_tongue.py
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -5,11 +5,11 @@ along a specific axis
 """
 
 
+import numpy as np
 def np_cat(mat1, mat2, axis=0):
     """
     Concatenates two ndarray matrices along
     a specific axis
     """
-    import numpy as np
     np_cat = np.concatenate((mat1, mat2), axis=axis)
     return np_cat
