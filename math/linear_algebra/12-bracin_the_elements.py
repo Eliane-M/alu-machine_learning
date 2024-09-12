f2b3557ad3d83f49290c4df50bc8837206afacb4
Eliane_M <munezeroeliane761@gmail.com>
2024-09-12 06:02:21 +0200

    algebra work

---
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+This function performs element-wise addition,
+subtraction, multiplication, and division
+"""
+
+
+def np_elementwise(mat1, mat2):
+    """
+    Performs element-wise addition,
+    subtraction, multiplication, and division
+    """
+    add = mat1 + mat2
+    
+    sub = mat1 - mat2
+
+    mul = mat1 * mat2
+
+    div = mat1 / mat2
+
+    return add, sub, mul, div
