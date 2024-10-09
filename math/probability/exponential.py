9e3da1f0c1fc1827896ec3237cba4330baa21931
Eliane_M <munezeroeliane761@gmail.com>
2024-10-09 17:08:05 +0200

    exponential

--- math/probability/exponential.py
+++ math/probability/exponential.py
@@ -8,4 +8,15 @@ exponential distribution
 class Exponential():
     """Represents an exponential distribution"""
     def __init__(self, data=None, lambtha=1.):
-        pass
+        """Class constructor for Exponential distribution."""
+        if data is None:
+            if lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
+            self.lambtha = float(lambtha)
+        else:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            # Calculate lambtha as the inverse of the mean of the data
+            self.lambtha = float(1 / (sum(data) / len(data)))
