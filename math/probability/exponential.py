fcb944887d1f203c1fcfa1b094d820bfa4a836f6
Eliane_M <munezeroeliane761@gmail.com>
2024-10-09 17:09:40 +0200

    exponential

--- math/probability/exponential.py
+++ math/probability/exponential.py
@@ -20,3 +20,23 @@ class Exponential():
                 raise ValueError("data must contain multiple values")
             # Calculate lambtha as the inverse of the mean of the data
             self.lambtha = float(1 / (sum(data) / len(data)))
+
+    def exp(self, x):
+        """Calculates the exponential of x using Taylor series."""
+        result = 1.0
+        term = 1.0
+        for i in range(1, 100):  # Increase terms for better precision
+            term *= x / i
+            result += term
+        return result
+
+    def pdf(self, x):
+        """Calculates the value of the PDF for a given time period x."""
+        if x < 0:
+            return 0
+
+        # PDF formula: f(x) = lambtha * e^(-lambtha * x)
+        exp_neg_lambtha_x = 1 / self.exp(self.lambtha * x)
+
+        pdf_value = self.lambtha * exp_neg_lambtha_x
+        return pdf_value
