e38a290763cc20bf9ee80a92bffb345fd7a20d00
Eliane_M <munezeroeliane761@gmail.com>
2024-10-17 13:05:31 +0200

    multinormal

--- math/multivariate_prob/multinormal.py
+++ math/multivariate_prob/multinormal.py
@@ -16,7 +16,7 @@ class MultiNormal():
         """
         class constructor
         """
-        if not isinstance(data, np.ndarray):
+        if not isinstance(data, np.ndarray) or len(data.shape) != 2:
             raise TypeError('data must be a 2D numpy.ndarray')
 
         # Validate data shape
