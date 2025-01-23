68a538e8a10142ca4a7d3c88c0f5205eec540f3c
Eliane_M <munezeroeliane761@gmail.com>
2025-01-23 18:57:42 +0200

    '0-neuron'

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -18,6 +18,6 @@ class Neuron:
         if nx < 1:
             raise ValueError('nx must be a positive integer')
 
-        self.W = np.random.randn(nx).tolist()
+        self.W = np.random.randn(1, nx)
         self.b = 0
         self.A = 0
