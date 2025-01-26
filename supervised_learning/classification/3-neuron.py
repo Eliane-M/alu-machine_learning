9637b43cce66c4cd162219585600f1920391cf00
Eliane-M <125857083+Eliane-M@users.noreply.github.com>
2025-01-26 08:31:48 +0200

    Add files via upload

---
+++ supervised_learning/classification/3-neuron.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+'''
+Defines a single neuron performing binary classification
+'''
+
+
+import numpy as np
+
+
+class Neuron:
+    '''
+    A single neuron performing binary classification
+    '''
+    def __init__(self, nx):
+        '''
+        Initializes the neuron with given weights and bias
+        Args:
+            weights (numpy.ndarray): The weights of the neuron
+            bias (float): The bias of the neuron
+        '''
+        if not isinstance(nx, int):
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        '''
+        Returns the weights of the neuron
+        Returns:
+            numpy.ndarray: The weights of the neuron
+        '''
+        return self.__W
+
+    @property
+    def b(self):
+        '''
+        Returns the bias of the neuron
+        Returns:
+            float: The bias of the neuron
+        '''
+        return self.__b
+
+    @property
+    def A(self):
+        '''
+        Returns the activation of the neuron
+        Returns:
+            float: The activation of the neuron
+        '''
+        return self.__A
+
+    def forward_prop(self, X):
+        '''
+        Calculates the forward propagation of the neuron
+        Args:
+            X (numpy.ndarray): The input data
+        Returns:
+            float: The activation of the neuron
+        '''
+        z = np.matmul(self.__W, X) + self.__b
+        self.__A = 1 / (1 + np.exp(-z))
+        return self.__A
+
+    def cost(self, Y, A):
+        '''
+        Calculates the cost of the model using logistic regression
+        '''
+        m = Y.shape[1]
+        cost = -1 / m * np.sum(Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A))
+        return cost
