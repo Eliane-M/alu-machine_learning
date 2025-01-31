6ebe542c2cbc7587816b65a3972e70b0a9b8b5ce
Eliane_M <munezeroeliane761@gmail.com>
2025-01-31 09:19:13 +0200

    train

--- supervised_learning/tensorflow/6-train.py
+++ supervised_learning/tensorflow/6-train.py
@@ -16,7 +16,7 @@ forward_prop = __import__('2-forward_prop').forward_prop
 def train(
         X_train, Y_train, X_valid, Y_valid, layer_sizes,
         activations, alpha, iterations, save_path="/tmp/model.ckpt"
-    ):
+):
     '''
     Builds, trains and saves a neural network classifier
     '''
@@ -25,31 +25,34 @@ def train(
     loss = calculate_loss(y, y_pred)
     accuracy = calculate_accuracy(y, y_pred)
     train_op = create_train_op(loss, alpha)
-    
+
     tf.add_to_collection("x", x)
     tf.add_to_collection("y", y)
     tf.add_to_collection("y_pred", y_pred)
     tf.add_to_collection("loss", loss)
     tf.add_to_collection("accuracy", accuracy)
     tf.add_to_collection("train_op", train_op)
-    
+
     init = tf.global_variables_initializer()
     saver = tf.train.Saver()
-    
+
     with tf.Session() as sess:
+        '''
+        Initialize variables and train the model
+        '''
         sess.run(init)
         for i in range(iterations + 1):
             train_loss, train_acc = sess.run([loss, accuracy], feed_dict={x: X_train, y: Y_train})
             valid_loss, valid_acc = sess.run([loss, accuracy], feed_dict={x: X_valid, y: Y_valid})
-            
+
             if i % 100 == 0 or i == iterations:
                 print(f"After {i} iterations:")
                 print(f"\tTraining Cost: {train_loss}")
                 print(f"\tTraining Accuracy: {train_acc}")
                 print(f"\tValidation Cost: {valid_loss}")
                 print(f"\tValidation Accuracy: {valid_acc}")
-                
+
             if i < iterations:
                 sess.run(train_op, feed_dict={x: X_train, y: Y_train})
-        
+
         return saver.save(sess, save_path)
