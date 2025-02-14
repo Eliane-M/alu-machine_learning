01ff7088c58b9b1d2925b310d026b29d92bbcbbb
Eliane_M <munezeroeliane761@gmail.com>
2025-02-14 08:23:04 +0200

    error analysis

---
+++ supervised_learning/error_analysis/4-f1_score.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+calculates the F1 score of a confusion matrix
+"""
+import numpy as np
+sensitivity = __import__('1-sensitivity').sensitivity
+precision = __import__('2-precision').precision
+
+
+def f1_score(confusion):
+    """Calculates the F1 score of a confusion matrix"""
+    sens_v = sensitivity(confusion)
+    prec_v = precision(confusion)
+
+    f1 = np.divide(2, np.power(sens_v, -1) + np.power(prec_v, -1))
+    return f1
---
+++ supervised_learning/error_analysis/README.md
@@ -0,0 +1,1 @@
+This repository is for error analysis using ML
